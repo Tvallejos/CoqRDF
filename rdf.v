ccaa2d3b8990c5e98cebe2a1b607d937cc008105
Tomas <tomas.vallejos12@gmail.com>
2021-06-29 18:36:00 -0400

    redefinition of triples and graph

--- rdf.v
+++ rdf.v
@@ -1,4 +1,5 @@
 From Coq Require Import Strings.String.
+From Coq Require Import Lists.ListSet.
 
 Inductive node : Type :=
   | Const (c : nat)
@@ -7,17 +8,25 @@ Inductive node : Type :=
 Check (Const 5) : node.
 Check (Var "x") : node.
 
-Inductive trpl : Type :=
+(*Inductive trpl : Type :=
   | triple (s p o : node).
+ *)
 
-(* Notation triple := (node * node * node). *)
-(* Definition triple := (node * node * node). *)
+Definition triple := (node * node * node)%type. 
 
-Check (triple (Const 5) (Const 1) (Const 8)): trpl. (* TODO why fails *)
-Check (triple (Var "x") (Const 1) (Var "foo")): trpl.
+Check ((Const 5), (Const 1),(Const 8)): triple. 
+Check ((Var "x"), (Const 1), (Var "foo")): triple.
 
-(* Check ((Const 5),(Const 2),(Const 1)). *)
 
-Inductive rdf : Type :=
-  | graph (triples : list trpl).
+(*
+   Inductive rdf : Type :=
+  | graph (triples : list triple).
+ *)
+Check set.
+Definition graph := set triple.
+Check graph.
 
+(* Check [((Const 5),(Const 5), (Const 5))].
+   Check (set_add ((Const 5),(Const 5), (Const 5)) empty_set). *)
+(* Check (set ((Const 5),(Const 5), (Const 5)),  ((Var "x"),(Const 1),(Const 2))  ) : graph.
+ *)

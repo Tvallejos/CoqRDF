2f419c5251ad6121f334a3fe7e2900698ef7ab61
Tomas <tomas.vallejos12@gmail.com>
2021-06-30 12:59:41 -0400

    [Model] eqb_node, maps of nodes (isomorphism)

--- src/Maps.v
+++ src/Maps.v
@@ -1,12 +1,11 @@
 From Coq Require Export Strings.String.
 From Coq Require Import Arith.Arith.
-Definition eqb_string (x y : string) : bool :=
-  if string_dec x y then true else false.
+From RDF Require Import Rdf.
 
 Definition total_map (A : Type) := node -> A.
 Definition t_empty {A : Type} (v : A) : total_map A :=
   (fun _ => v).
 
 Definition t_update {A : Type} (m : total_map A)
-                    (x : string) (v : A) :=
-  fun x' => if eqb_string x x' then v else m x'. 
+                    (n : node) (v : A) :=
+  fun n' => eqb_node n n' then v else m n'.

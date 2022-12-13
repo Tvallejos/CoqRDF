d6d860a8912664eba8bd85253f59a640dc954e2b
Tomas <tomas.vallejos12@gmail.com>
2022-12-13 17:59:05 -0300

    some golf on util lemmas and changing destruct for case

--- src/Util.v
+++ src/Util.v
@@ -3,12 +3,6 @@ Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
-(* Several problems here:
-- why naming a hypothesis if you generalize it right away)
-- think about the statement of the lemma (here equality/equivalence holds, not only implication)
-- use lemmas in the library (no need for induction here)
-*)
-
 Lemma all_undup (T : eqType) (s : seq T) p : all p (undup s) = all p s.
 Proof.
   suffices /eq_all_r -> : (undup s) =i s by [].
@@ -23,47 +17,34 @@ Qed.
 Definition undup_in (T : eqType) (t : T) (s : seq T) :
   t \in s -> t \in undup s.
 Proof. elim: s=> [//| h hs IHts].
-       + rewrite in_cons. case orP; last by [].
-         case=> [ /eqP <- | ih] _.
-       - destruct (t \in hs) eqn:E; rewrite /undup E.
-         * by apply IHts.
-         * by rewrite mem_head.
-       - destruct (h \in hs) eqn:E;rewrite /undup E.
-         apply: IHts ih.
-         apply: inweak. apply: IHts ih.
+       + rewrite /= in_cons. case/orP.
+         move=> /eqP<-.
+       - case e: (t \in hs); first by apply: IHts e.
+         by rewrite mem_head.
+       - case e: (h \in hs); first by apply IHts.
+         by move=> /IHts tin_hs; apply: inweak tin_hs.
 Qed.
 
 Definition undup_idem (T : eqType) (s : seq T) :
   undup (undup s) = undup s.
-Proof. elim: s=> [//| t ts IHts] /=.
-       destruct (t \in ts) eqn:E.
-       + apply IHts.
-       + have h: (t \in (undup ts) = false).
-         by rewrite -E; apply mem_undup.
-         by rewrite /= h IHts.
+Proof.
+  elim: s=> [//| t ts IHts] /=.
+  case e: (t \in ts).
+  + by rewrite IHts.
+  + by rewrite /= mem_undup e IHts. 
 Qed.
 
 Definition undup_cat_r (T: eqType) (s q : seq T) :
   undup (s ++ undup q) = undup (s ++ q).
 Proof.
-  elim: s=> [//| aq qs IHqs] /=.
-  + apply undup_idem.
-  + destruct (aq \in qs) eqn:E; rewrite !mem_cat E /=.
-  - apply IHqs.
-  - destruct (aq \in q) eqn:E2.
-    have -> : (aq \in undup q) = true. rewrite -E2; apply mem_undup.
-    apply IHqs.
-    have -> : (aq \in undup q) = false. rewrite -E2; apply mem_undup.
-    rewrite IHqs; done.
+  elim: s=> [//| aq qs IHqs] /=; first exact: undup_idem.
+  by rewrite !mem_cat mem_undup IHqs.
 Qed.
 
 Definition undup_cat_l (T: eqType) (s q : seq T) :
   undup (undup s ++ q) = undup (s ++ q).
 Proof.
-  elim: s=> [//| aq qs IHqs]; rewrite (undup_cat (aq :: qs)) /=.
-  case e: (aq \in qs).
-  + by rewrite IHqs undup_cat.
-  + by rewrite undup_cat -cat1s undup_cat_r !cat1s {1}/undup e.
+  by rewrite !undup_cat undup_idem.
 Qed.
 
 Lemma empty_permutations (T:eqType) : @permutations T [::] = [:: [::]]. Proof. by []. Qed.
@@ -73,7 +54,7 @@ Lemma map_inv (T U: eqType) (s:seq T) (f: T -> U):
 Proof. elim : s => [| a t IHts] u /=; first by rewrite in_nil.
        rewrite in_cons; case/orP => [/eqP -> | y].
        - by exists a.
-       - by apply: IHts; apply y.
+              - by apply: IHts y.
 Qed.
 
 Lemma foldl_op (disp: unit) (T: porderType disp) (l: seq T) (x0 : T) : foldl Order.min x0 l = x0 \/ foldl Order.min x0 l \in l.
@@ -104,7 +85,7 @@ Qed.
 
 Lemma in_zip_sym (S T : eqType) (ss : seq S) (ts : seq T) s t:
   (s,t) \in zip ss ts = ((t, s) \in zip ts ss).
-Proof. 
+Proof.
   elim: ts ss=> [| t' ts' IHts] ss; first by case ss.
   + case: ss=> [| s' ss']; first by rewrite zip0s.
   - rewrite /= !in_cons. f_equal; first by rewrite !xpair_eqE Bool.andb_comm.
@@ -135,12 +116,10 @@ Qed.
 
 Lemma zip_uniq_l (S T : eqType) (ss : seq S) (ts : seq T) : uniq ss -> uniq (zip ss ts).
 Proof.
-  elim: ts ss=> [| hd2 tl2 IHtl2] ss uniq_ss; first by case ss.
+  elim: ts ss=> [| t' ts' IHts] ss uniq_ss; first by case ss.
   + case: ss uniq_ss => [| s ss']; first by rewrite zip0s.
-    move=> uniq_ss; rewrite /=; apply /andP; split.
-    move: uniq_ss=> /==> /andP [nin_ss' _]; apply (notin_zip_l _ _ nin_ss').
-    apply: IHtl2;
-      move: uniq_ss => /==> /andP [_ uniq_ss]. exact: uniq_ss.
+    rewrite /==> /andP[nin_ss uniq_ss]; apply /andP; split; first by apply (notin_zip_l _ _ nin_ss).
+    by apply: IHts uniq_ss.
 Qed.
 
 Lemma zip_uniq_r (S T : eqType) (ss : seq S) (ts : seq T) : uniq ts -> uniq (zip ss ts).
